cdcb2109045da519560bde209df5d9495489f551
SPai <sanjeev@rishicorp.com>
2021-03-11 09:53:08 -0600

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {CrisisListComponent} from './crisis-list/crisis-list.component';
+import {HeroListComponent} from './hero-list/hero-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrisisListComponent,
+    HeroListComponent
   ],
   imports: [
     BrowserModule
